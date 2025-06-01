3362484621b47c86de35a75eb704b46b402a7767
Cynthia <cynthia.abuya@student.moringaschool.com>
2025-06-01 15:28:36 +0300

    feat(models): expose Recipe, Ingredient, MealPlan,  and association table via __init__.py

---
+++ meal_planner/models/__init__.py
@@ -0,0 +1,6 @@
+from .recipe import Recipe
+from .ingredient import Ingredient
+from .meal_plan import MealPlan
+from .recipe_ingredient import recipe_ingredient
+
+__all__ = ["Recipe", "Ingredient", "MealPlan", "recipe_ingredient"]
